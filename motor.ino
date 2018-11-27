d9997566baf9d1a10f18b8e4b6a439253d7b37de
Mihai Popescu <34679869+MihaiPopescu1985@users.noreply.github.com>
2018-11-27 13:17:53 +0200

    Update motor.ino

--- motor.ino
+++ motor.ino
@@ -1,128 +1,111 @@
 #include <SoftwareSerial.h>
 
-#define ENABLE_LEFT 9
-#define FWD_LEFT 8
-#define BWD_LEFT 7
+#define ENABLE_FWD_BWD 9
+#define BWD 8
+#define FWD 7
 
-#define ENABLE_RIGHT 6
-#define FWD_RIGHT 5
-#define BWD_RIGHT 4
+#define ENABLE_LEFT_RIGHT 6
+#define RIGHT 5
+#define LEFT 4
 
 SoftwareSerial BTSerial(2, 3);
 int state;
 int carSpeed;
 
-void carStop(){
-  digitalWrite(FWD_LEFT, LOW);
-  digitalWrite(FWD_RIGHT, LOW);
-  digitalWrite(BWD_LEFT, LOW);
-  digitalWrite(BWD_RIGHT, LOW);
-}
-
-void moveFwd(int speed) {
-  analogWrite(ENABLE_LEFT, speed);
-  analogWrite(ENABLE_RIGHT, speed);
-
-  digitalWrite(FWD_LEFT, LOW);
-  digitalWrite(FWD_RIGHT, LOW);
-  digitalWrite(BWD_LEFT, HIGH);
-  digitalWrite(BWD_RIGHT, HIGH);
-}
-
-void moveBwd(int speed) {
-  analogWrite(ENABLE_LEFT, speed);
-  analogWrite(ENABLE_RIGHT, speed);
-
-  digitalWrite(BWD_LEFT, LOW);
-  digitalWrite(BWD_RIGHT, LOW);
-  digitalWrite(FWD_LEFT, HIGH);
-  digitalWrite(FWD_RIGHT, HIGH);
-}
-
-void moveLeft(){
-  analogWrite(ENABLE_RIGHT, 255);
-  analogWrite(ENABLE_LEFT, 255);
-  
-  digitalWrite(FWD_RIGHT, LOW);
-  digitalWrite(BWD_RIGHT, HIGH);
-  digitalWrite(FWD_LEFT, HIGH);
-  digitalWrite(BWD_LEFT, LOW);
-  
-  delay(500);
-  disable();
-}
-
-void moveRight(){
-  analogWrite(ENABLE_LEFT, 255);
-  analogWrite(ENABLE_RIGHT, 255);
-  
-  digitalWrite(BWD_LEFT, HIGH);
-  digitalWrite(FWD_RIGHT, HIGH);
-  digitalWrite(FWD_LEFT, LOW);
-  digitalWrite(BWD_RIGHT, LOW);
-  
-  delay(500);
-  disable();
-}
-
-void disable() {
-  analogWrite(ENABLE_LEFT, 0);
-  digitalWrite(FWD_LEFT, LOW);
-  digitalWrite(BWD_LEFT, LOW);
-  analogWrite(ENABLE_RIGHT, 0);
-  digitalWrite(FWD_RIGHT, LOW);
-  digitalWrite(BWD_RIGHT, LOW);
-}
-
 void setup() {
-  pinMode(ENABLE_LEFT, OUTPUT);
-  pinMode(FWD_LEFT, OUTPUT);
-  pinMode(BWD_LEFT, OUTPUT);
+  pinMode(ENABLE_FWD_BWD, OUTPUT);
+  pinMode(FWD, OUTPUT);
+  pinMode(BWD, OUTPUT);
 
-  pinMode(ENABLE_RIGHT, OUTPUT);
-  pinMode(FWD_RIGHT, OUTPUT);
-  pinMode(BWD_RIGHT, OUTPUT);
+  pinMode(ENABLE_LEFT_RIGHT, OUTPUT);
+  pinMode(LEFT, OUTPUT);
+  pinMode(RIGHT, OUTPUT);
 
   Serial.begin(9600);
   BTSerial.begin(9600);
   state = 0;
 }
 
+void moveFwd() {
+  analogWrite(ENABLE_LEFT_RIGHT, 0);
+  analogWrite(ENABLE_FWD_BWD, 255);
+
+  digitalWrite(BWD, LOW);
+  digitalWrite(FWD, HIGH);
+}
+
+void moveBwd() {
+  analogWrite(ENABLE_LEFT_RIGHT, 0);
+  analogWrite(ENABLE_FWD_BWD, 255);
+
+  digitalWrite(FWD, LOW);
+  digitalWrite(BWD, HIGH);
+}
+
+void turnLeft() {
+  analogWrite(ENABLE_LEFT_RIGHT, 255);
+
+  digitalWrite(RIGHT, LOW);
+  digitalWrite(LEFT, HIGH);
+}
+
+void turnRight() {
+  analogWrite(ENABLE_LEFT_RIGHT, 255);
+
+  digitalWrite(LEFT, LOW);
+  digitalWrite(RIGHT, HIGH);
+}
+
+void stopMoving() {
+  analogWrite(ENABLE_FWD_BWD, 0);
+  analogWrite(ENABLE_LEFT_RIGHT, 0);
+}
+
 void loop() {
-  if(BTSerial.available()){
+  if(BTSerial.available()) {
     state = BTSerial.read();
-    Serial.write(state);
   }
-  
-  switch(state){
-    case '1':
-      moveFwd(200);
+  switch (state) {
+    case '1' :
+      moveFwd();
       break;
-    case '2':
-      moveBwd(200);
+    case '2' :
+      moveBwd();
       break;
-    case '3':
-      moveLeft();
-      state = '0';
+    case '3' :
+      turnLeft();
       break;
-    case '4':
-      moveRight();
-      state = '0';
+    case '4' :
+      turnRight();
       break;
-    case '5':
-      moveFwd(250);
+    case '5' :
+      state = '1';
       break;
-    case '6':
-      moveBwd(250);
+    case '6' :
+      state = '2';
       break;
-    case '7':
+    case '7' :
       state = '3';
       break;
-    case '8':
+    case '8' :
       state = '4';
       break;
-    case '0':
-      carStop();
+    default :
+      stopMoving();
       break;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
