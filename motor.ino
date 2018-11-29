57a6d3b6bed7690e8d7cc9a0212ebdd5557cdee9
Mihai Popescu <34679869+MihaiPopescu1985@users.noreply.github.com>
2018-11-29 08:21:09 +0200

    Update motor.ino

--- motor.ino
+++ motor.ino
@@ -1,3 +1,19 @@
+/*
+ * The protocol used to communicate :
+ * '1' - the car is moving forward in low speed
+ * '2' - the car is moving backward in low speed
+ * '3' - front weels are turning left
+ * '4' - front weels are turning right
+ * '5' - the care is moving forward in high speed
+ * '6' - the car is moving backward in high speed
+ * '7' - front weels are turning left
+ * '8' - front weels are turning right
+ * 'a' - front LEDs are turning on
+ * 'b' - front LEDs are turning off
+ * 'c' - the buzzer is turning on for 1 second
+ */
+
+
 #include <SoftwareSerial.h>
 
 #define ENABLE_FWD_BWD 9
@@ -7,6 +23,9 @@
 #define ENABLE_LEFT_RIGHT 6
 #define RIGHT 5
 #define LEFT 4
+
+#define LED 10
+#define BUZZER 11
 
 SoftwareSerial BTSerial(2, 3);
 int state;
@@ -21,6 +40,9 @@ void setup() {
   pinMode(LEFT, OUTPUT);
   pinMode(RIGHT, OUTPUT);
 
+  pinMode(LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+
   Serial.begin(9600);
   BTSerial.begin(9600);
   state = 0;
@@ -28,7 +50,7 @@ void setup() {
 
 void moveFwd() {
   analogWrite(ENABLE_LEFT_RIGHT, 0);
-  analogWrite(ENABLE_FWD_BWD, 255);
+  analogWrite(ENABLE_FWD_BWD, carSpeed);
 
   digitalWrite(BWD, LOW);
   digitalWrite(FWD, HIGH);
@@ -36,7 +58,7 @@ void moveFwd() {
 
 void moveBwd() {
   analogWrite(ENABLE_LEFT_RIGHT, 0);
-  analogWrite(ENABLE_FWD_BWD, 255);
+  analogWrite(ENABLE_FWD_BWD, carSpeed);
 
   digitalWrite(FWD, LOW);
   digitalWrite(BWD, HIGH);
@@ -65,11 +87,19 @@ void loop() {
   if(BTSerial.available()) {
     state = BTSerial.read();
   }
+
+  if(Serial.available()) {
+    state = Serial.read();
+    Serial.write(state);
+  }
+  
   switch (state) {
     case '1' :
+      carSpeed = 100;
       moveFwd();
       break;
     case '2' :
+      carSpeed = 100;
       moveBwd();
       break;
     case '3' :
@@ -79,10 +109,12 @@ void loop() {
       turnRight();
       break;
     case '5' :
-      state = '1';
+      carSpeed = 255;
+      moveFwd();
       break;
     case '6' :
-      state = '2';
+      carSpeed = 255;
+      moveBwd();
       break;
     case '7' :
       state = '3';
@@ -90,22 +122,19 @@ void loop() {
     case '8' :
       state = '4';
       break;
+    case 'a' :
+      digitalWrite(LED, HIGH);
+      break;
+    case 'b' :
+      digitalWrite(LED, LOW);
+      break;
+    case 'c' :
+      digitalWrite(BUZZER, HIGH);
+      delay(1000);
+      digitalWrite(BUZZER, LOW);
+      break;
     default :
       stopMoving();
       break;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
